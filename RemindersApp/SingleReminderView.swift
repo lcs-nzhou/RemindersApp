6ff8d7d2d0b7572b83c3c983ca1e3f7966e0d244
Nina Zhou <nzhou@lcs.on.ca>
2023-11-03 09:59:38 -0400

    SingleReminderView

---
+++ RemindersApp/SingleReminderView.swift
@@ -0,0 +1,18 @@
+//
+//  SingleReminderView.swift
+//  RemindersApp
+//
+//  Created by Shuyu Zhou on 2023-11-03.
+//
+
+import SwiftUI
+
+struct SingleReminderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SingleReminderView()
+}
