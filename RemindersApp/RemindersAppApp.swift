7bbdd173317f5a12e78d55cb97b3f1d12f3598f3
Nina Zhou <nzhou@lcs.on.ca>
2023-11-03 09:22:48 -0400

    Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Shuyu Zhou on 2023-11-03.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
